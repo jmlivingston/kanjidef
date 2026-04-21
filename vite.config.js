627b190d9a7a5b99b0deab87b9245c7332051f53
John Livingston <github@jml.simplelogin.com>
2026-04-21 07:47:19 -0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const basePath = repositoryName?.endsWith('.github.io')
+  ? '/'
+  : repositoryName
+    ? `/${repositoryName}/`
+    : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: basePath,
   plugins: [react()],
 })
