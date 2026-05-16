07482c30ef55ad2190c0dee77b7a478348f81255
John Livingston <github@jml.simplelogin.com>
2026-05-16 12:30:42 -0700

    Tweak

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ function App() {
       <div className="row justify-content-center">
         <div className="col-12 col-lg-10">
           <div className="d-flex justify-content-between align-items-center mb-3">
-            <h1 className="mb-0">Kanji Search (Top {kanjiData.meta.count.toLocaleString('en-US')})</h1>
+            <h1 className="mb-0">Kanji (Top {kanjiData.meta.count.toLocaleString('en-US')})</h1>
             <div className="form-check form-switch mb-0 ms-3">
               <input
                 id="darkModeToggle"
